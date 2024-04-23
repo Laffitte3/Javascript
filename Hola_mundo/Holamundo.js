db19e7907f0127b850bafb621e2fa11cf6e8b6ed
Alejandro <alejandromerlina324@gmail.com>
2024-04-22 22:50:15 -0700

    Variables, no termiando

--- Hola_mundo/Holamundo.js
+++ Hola_mundo/Holamundo.js
@@ -2,4 +2,10 @@
 alert("Hola World");
 alert("Mi nombre es Alejandro");
 
-document.write("Hola mundo desde fichero externo")
+
+//Imprime cosas en la pantalla
+document.write("Hola mundo desde fichero externo");
+
+console.log("Muestra esto en la consola: ");
+console.log(44+5);
+
---
+++ Variables/01.variables.js
@@ -0,0 +1,16 @@
+
+
+
+var pregunta = "Mi nombre es:";
+var name = "Pedro";
+var birth_date = 2001;
+
+var nombre_y_nacimiento = name+" nacio en "+birth_date;
+
+document.write(pregunta);
+document.write(name);
+document.write(birth_date);
+
+console.log(pregunta,name,birth_date);
+
+alert(nombre_y_nacimiento);
